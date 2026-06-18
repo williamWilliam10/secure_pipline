48fd0ff7ce81940392fe1592026b2b461025584c
William Lowe <lowewilliam237@gmail.com>
2026-06-18 12:13:12 +0200

    ajout job semgrep avec commentaires + fichier test vulnerable

---
+++ test-vuln.py
@@ -0,0 +1,9 @@
+# test-vuln.py
+import sqlite3
+
+def get_user(user_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE id = " + user_id
+    cursor.execute(query)
+    return cursor.fetchone()
