afeb208d0c829c1be30e08048babb5d828e5594e
William Lowe <lowewilliam237@gmail.com>
2026-06-18 14:10:58 +0200

    hardcoded-password-assignment

--- test-vuln.py
+++ test-vuln.py
@@ -2,6 +2,7 @@
 import sqlite3
 
 def get_user(user_id):
+    admin_password = "SuperSecret123!"
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     query = "SELECT * FROM users WHERE id = " + user_id
